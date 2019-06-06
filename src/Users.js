7ca409357bd0f3263f4500ce4bbf67e11c9214f7
pawelmisiak <thepawelmisiak@gmail.com>
2019-06-06 10:49:01 -0400

    added Users and Contact components

---
+++ src/Users.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Users extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <h1>Users</h1>;
+  }
+}
+
+export default Users;
